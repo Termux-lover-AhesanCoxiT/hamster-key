fdf77fda5661d79044e646f5cd5a4b1918141643
iiT'S AheSaNCoxiT <119955773+Termux-lover-AhesanCoxiT@users.noreply.github.com>
2024-09-06 21:34:47 +0600

    Update script.js

--- script.js
+++ script.js
@@ -157,7 +157,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let selectedGame = null;
 
     sourceCode.addEventListener('click', () => {
-        window.open('https://github.com/ShafiqSadat/HamsterKeyGenWeb', '_blank');
+        window.open('https://www.facebook.com/iTzCoxitBoyAhesan', '_blank');
     });
 
     gameOptions.forEach(option => {
